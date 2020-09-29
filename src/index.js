b19b6112f91fe2c89e2bd66c8df74a0cdadd7797
Ablan Abkenov <abkenov.ablan@gmail.com>
2020-09-30 03:25:06 +0600

    feat: add min, max, avg methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || !array.length)
+    return 0
+
+  let min = Infinity
+
+  for (let num of array) {
+    if (num < min) {
+      min = num
+    }
+  }
+
+  return min
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || !array.length)
+    return 0
+
+  let max = (-1) * Infinity
+
+  for (let num of array) {
+    if (num > max) {
+      max = num
+    }
+  }
+
+  return max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || !array.length)
+    return 0
+
+  let sum = 0
+
+  for (let num of array) {
+    sum += num
+  }
+
+  return sum / array.length
 }
